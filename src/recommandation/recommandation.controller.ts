0fa69265dcd9f15816f9ac234e6cf2b3c4ddd779
abdulmoizazher <abdulmoizazher@gmail.com>
2025-04-02 10:57:01 +0500

    testing youtube api

--- src/recommandation/recommandation.controller.ts
+++ src/recommandation/recommandation.controller.ts
@@ -13,48 +13,18 @@ export class RecommandationController {
 
 @UseGuards(JwtAuthGuard)
 @Get('current')
-async user_recommandation (@Request() req):Promise <any | string>{
+async user_recommandation (@Request() req):Promise <any | string>
+{
 
-    const preference = await this.UserRec.get_user_recommandation(req.user.userId);
-    console.log("Full preference object:", preference); // Check if keys exist
-    console.log("All keys:", Object.keys(preference));
-    const positiveEmotions = [
-        "admiration", "amusement", "approval", "caring", "curiosity",
-        "desire", "excitement", "gratitude", "joy", "love",
-        "optimism", "pride", "relief"
-      ];
-      const negativeEmotions = [
-        "anger", "annoyance", "disappointment", "disapproval", "disgust",
-        "embarrassment", "fear", "grief", "nervousness", "remorse",
-        "sadness", "stress", "anxiety", "depression", "suicidal"
-      ];
-    
-      
-      const neutralEmotions = [
-        "confusion", "realization", "surprise", "unknown", "neutral"
-      ];
-
-      
-    const sentiment = "joy";
-
-    if(positiveEmotions.includes(sentiment)){
-        return preference.mood_happy;
+        return     this.UserRec.user_recommandation(req.user.userId)
         
-      }
-
-    else if(negativeEmotions.includes(sentiment)){
-        return preference.mood_sad;
-      }
-
-    else if(neutralEmotions.includes(sentiment)){
-        return preference.mood_confused;
-      }
-
-      
 
 }
 
-
+@Get('test')
+async test (){
+  return this.UserRec.test();
+}
 
 }
 
--- src/recommandation/recommandation.service.ts
+++ src/recommandation/recommandation.service.ts
@@ -1,16 +1,84 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
 
 @Injectable()
 export class RecommandationService {
+
+    private readonly youtubeApiUrl: string;
+    private readonly youtubeApiKey: string;
+
+ 
+    
     constructor(
         private readonly userSrv: UserService,
-    ) { }
+        private readonly configService: ConfigService
+        
+    ) { 
+        this.youtubeApiUrl = this.configService.get<string>('YOUTUBE_API_URL');
+        this.youtubeApiKey = this.configService.get<string>('YOUTUBE_API_KEY');
+    }
 
 
-    async get_user_recommandation(userId: any) {
+    async user_recommandation(userId: any) {
 
 
-        return this.userSrv.get_user_preference(userId);
+
+        const user_preference = await this.userSrv.get_user_preference(userId);
+        const positiveEmotions = [
+            "admiration", "amusement", "approval", "caring", "curiosity",
+            "desire", "excitement", "gratitude", "joy", "love",
+            "optimism", "pride", "relief"
+        ];
+        const negativeEmotions = [
+            "anger", "annoyance", "disappointment", "disapproval", "disgust",
+            "embarrassment", "fear", "grief", "nervousness", "remorse",
+            "sadness", "stress", "anxiety", "depression", "suicidal"
+        ];
+
+
+        const neutralEmotions = [
+            "confusion", "realization", "surprise", "unknown", "neutral"
+        ];
+
+
+        const sentiment = "joy";
+
+        if (positiveEmotions.includes(sentiment)) {
+            return user_preference.mood_happy;
+
+        }
+
+        else if (negativeEmotions.includes(sentiment)) {
+            return user_preference.mood_sad;
+        }
+
+        else if (neutralEmotions.includes(sentiment)) {
+            return user_preference.mood_confused;
+        }
+
+
+
+    }
+
+    async test(){
+        
+        const response = await axios.get(this.youtubeApiUrl, {
+            params: {
+              key: this.youtubeApiKey,
+              q: 'joy',
+              part: 'snippet',
+              maxResults: 10,
+              type: 'video',
+            },
+        });
+        return response.data.items.map(video => ({
+            title: video.snippet.title,
+            videoId: video.id.videoId,
+            thumbnail: video.snippet.thumbnails.high.url,
+            channelTitle: video.snippet.channelTitle,
+            publishedAt: video.snippet.publishedAt,
+          }));
     }
 }
